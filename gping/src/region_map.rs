8abb126b217c6b2ec08d1fddbf821f61d2b0e42b
Tom Forbes <tom@tomforb.es>
2024-12-15 15:06:12 +0000

    Support GCP cloud endpoints

--- gping/src/region_map.rs
+++ gping/src/region_map.rs
@@ -3,6 +3,8 @@ type Host = String;
 pub fn try_host_from_cloud_region(query: &str) -> Option<Host> {
     match query.split_once(':') {
         Some(("aws", region)) => Some(format!("ec2.{region}.amazonaws.com")),
+        Some(("gcp", "")) => Some("cloud.google.com".to_string()),
+        Some(("gcp", region)) => Some(format!("storage.{region}.rep.googleapis.com")),
         _ => None,
     }
 }
@@ -16,6 +18,18 @@ mod tests {
         assert_eq!(
             try_host_from_cloud_region("aws:eu-west-1"),
             Some("ec2.eu-west-1.amazonaws.com".to_string())
+        );
+    }
+
+    #[test]
+    fn test_host_from_gcp() {
+        assert_eq!(
+            try_host_from_cloud_region("gcp:me-central2"),
+            Some("storage.me-central2.rep.googleapis.com".to_string())
+        );
+        assert_eq!(
+            try_host_from_cloud_region("gcp:"),
+            Some("cloud.google.com".to_string())
         );
     }
 
